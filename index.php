45a0b7576d5e7c7da40810a0104b0701549fef2e
GenLR <162552154+GenLR@users.noreply.github.com>
2025-11-03 15:08:35 +0800

    Refactor index.php to improve structure and readability

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
 session_start();
 include('includes/connection.php');
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-	
-<!-- login23:11-->
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <link rel="icon" href="admin\assets\img\logo.jpg" type="image/x-icon">
-    <title>CLOCKEE</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <!--[if lt IE 9]>
-		<script src="assets/js/html5shiv.min.js"></script>
-		<script src="assets/js/respond.min.js"></script>
-	<![endif]-->
-</head>
-<?php
 if(isset($_REQUEST['login']))
 {
     $username = mysqli_real_escape_string($connection,$_REQUEST['username']);
@@ -50,6 +31,24 @@ if(isset($_REQUEST['login']))
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	
+<!-- login23:11-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <link rel="icon" href="admin\assets\img\logo.jpg" type="image/x-icon">
+    <title>CLOCKEE</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <!--[if lt IE 9]>
+		<script src="assets/js/html5shiv.min.js"></script>
+		<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
 <body>
     <div class="main-wrapper account-wrapper">
         <div class="account-page">
